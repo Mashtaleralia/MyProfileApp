ae2573e5bdadf1f51f44cb827b3c2ed844980ddb
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-08-01 10:32:32 +0300

    Initial Commit

---
+++ MyProfileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProfileApp
+//
+//  Created by Admin on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
